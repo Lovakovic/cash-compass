5f3e251d07edff9730b0df9528858a61a7c2d8ba
Lovakovic <lovakovdario@gmail.com>
2023-12-26 19:36:43 +0100

    Change CORS policy to fit the registered domain

--- apps/backend/src/config/cors.config.ts
+++ apps/backend/src/config/cors.config.ts
@@ -1,4 +1,8 @@
-export const corsConfig = {
-  origin: 'http://localhost:4200',
-  credentials: true,
+export const corsConfig = () => {
+  const isProduction = process.env.NODE_ENV === 'production'
+  const productionUrls = ['https://budget-compass.com', 'https://www.budget-compass.com'];
+
+  return {
+    origin: isProduction ? productionUrls : 'http://localhost:4200',    credentials: true,
+  }
 }
