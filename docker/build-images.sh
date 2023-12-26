5a4f2b8be2387ac088d84acb38145e3af8a305c1
Lovakovic <lovakovdario@gmail.com>
2023-12-26 21:52:58 +0100

    Create a ci-cd.yml workflow

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -5,50 +5,30 @@ set -e
 # Change the context to the workspace root directory
 cd "$(dirname "$0")/.."
 
-echo "Select the image(s) to build:"
-echo "1) Frontend"
-echo "2) Backend"
-echo "3) Both Frontend and Backend"
+# Set the tag for the build
+tag="latest"
 
-read -r -p "Enter your choice (1/2/3): " choice
+# Paths to Dockerfiles and Artifact Registry
+frontend_dockerfile="apps/cash-compass/Dockerfile"
+backend_dockerfile="apps/backend/Dockerfile"
+frontend_registry_path="europe-west8-docker.pkg.dev/cashcompass-409018/frontend-images"
+backend_registry_path="europe-west8-docker.pkg.dev/cashcompass-409018/backend-images"
 
-read -r -p "Enter the tag for the build (leave blank for 'latest'): " tag
-
-if [[ -z "$tag" ]]; then
-  tag="latest"
-fi
-
-read -r -p "Do you want to push the images to Google Artifact Registry? (y/n): " push_choice
-
+# Function to build and push image
 build_and_push() {
   image_name=$1
   dockerfile_path=$2
-  artifact_registry_path=$3
+  registry_path=$3
 
   echo "Building $image_name image..."
-  docker build -t "$artifact_registry_path/$image_name:$tag" -f "$dockerfile_path" .
+  docker build -t "$registry_path/$image_name:$tag" -f "$dockerfile_path" .
 
-  if [[ "$push_choice" == "y" ]]; then
-    echo "Pushing $image_name image to Google Artifact Registry..."
-    docker push "$artifact_registry_path/$image_name:$tag"
-  fi
+  echo "Pushing $image_name image to Google Artifact Registry..."
+  docker push "$registry_path/$image_name:$tag"
 }
 
-case $choice in
-  1)
-    build_and_push "cash-compass-frontend" "apps/cash-compass/Dockerfile" "europe-west8-docker.pkg.dev/cashcompass-409018/frontend-images"
-    ;;
-  2)
-    build_and_push "cash-compass-backend" "apps/backend/Dockerfile" "europe-west8-docker.pkg.dev/cashcompass-409018/backend-images"
-    ;;
-  3)
-    build_and_push "cash-compass-frontend" "apps/cash-compass/Dockerfile" "europe-west8-docker.pkg.dev/cashcompass-409018/frontend-images"
-    build_and_push "cash-compass-backend" "apps/backend/Dockerfile" "europe-west8-docker.pkg.dev/cashcompass-409018/backend-images"
-    ;;
-  *)
-    echo "Invalid choice. Please run the script again and select 1, 2, or 3."
-    exit 1
-    ;;
-esac
+# Build and push frontend and backend images
+build_and_push "cash-compass-frontend" "$frontend_dockerfile" "$frontend_registry_path"
+build_and_push "cash-compass-backend" "$backend_dockerfile" "$backend_registry_path"
 
 echo "Process complete."
